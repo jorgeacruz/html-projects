ba61da7f0131a4a63f0de8468e22fc5531f620ca
jorgeacruz <j.alexcruz@gmail.com>
2022-08-23 14:09:08 -0300

    Update files

---
+++ Calc_IMC/script.js
@@ -0,0 +1,33 @@
+
+
+var peso;
+var altura;
+var resultado;
+
+function calcIMC() {
+    event.preventDefault();
+
+    peso = document.getElementById("peso").value;
+    altura = document.getElementById("altura").value;
+    imc = peso / (altura * altura);
+
+    resultado = document.getElementById('resultado');
+
+    if(imc < 17){
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'
+      }else if(imc > 17 && imc <= 18.49){
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está Abaixo do peso!';
+      }else if(imc >= 18.5 && imc < 24.99){
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está no peso ideal!';
+      }else if(imc > 25 && imc <= 29.99){
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você está acima do peso!';
+      }else if(imc >= 30){
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado Obesidade!';
+      }
+    console.log(peso);
+    console.log(altura);
+    console.log(imc);
+    console.log(resultado);
+
+
+}
